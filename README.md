1d083400f28a69fb0102590f6c14a71685d3fd18
doktordos <doktordos@hotmail.com>
2015-12-06 18:00:33 +0100

    several files added to git

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # kletter.tracker-for-android
 
-test-commit
+Folgende Features sind geplant: 
+•	Login 
+•	QR-Code Reader 
+•	Tracking von Routen 
+•	Manuelles Tracking von Routen 
+•	Übersicht bereits getrackter Routen 
+•	Bewertung einer getrackten Route 
+•	Bewertungen von anderen Usern einsehen 
+
+Version 0.3 Beta:
+
+Design von iOS auf Android portiert
+LoginActivity, HomeActivity, ManuellActivity, ActivitiesActivity, QRCodeActivity implementiert
+
+Was geht nicht:
+
+Anbindung an REST-Backend
+QRCode Scanner
+Versteckte Malware
---
+++ app/src/main/java/ims/fhj/at/klettertracker/ActivitiesActivity.java
@@ -0,0 +1,71 @@
+package ims.fhj.at.klettertracker;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ActivitiesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activities);
+
+        String title = (String)getIntent().getExtras().get("title");
+        setTitle(title);
+
+        List valueList = new ArrayList<String>();
+        valueList.add("Dummy Track 1");
+        valueList.add("Dummy Track 2");
+        valueList.add("Dummy Track 3");
+
+        ListAdapter adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, valueList);
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                goToDetails();
+            }
+        });
+    }
+
+    public void goToDetails() {
+        Intent intent = new Intent(this, DetailsActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_activities, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
